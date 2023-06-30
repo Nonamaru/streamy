4d7e95494c519d1bf5ec76036256f5b79b87f88d
Nonamaru <zykinf@gmail.com>
2023-06-30 18:59:47 +0500

    home

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,12 @@ export default function Componet() {
   return (
     <View style={styles.web}>
       <View style={styles.viewContainer}>
-        <Text>suka {width}</Text>
+        <View style={styles.header}>
+          Header
+        </View>
+        <View style={styles.content}>
+          Content
+        </View>
       </View>
     </View>
   );
@@ -15,11 +20,27 @@ export default function Componet() {
 const styles = StyleSheet.create({
   web: {
     border: '1px solid black;',
-    flex: 1,
+    // flex: 1,
+    width: '100vw',
+    height: '100vh',
+    border: '1px solid blue',
   },
   viewContainer: {
-    border: width <= 1800 ? '1px solid black' : '2px solid red',
-
-  }
-
+    // border: width <= 1800 ? '1px solid black' : '2px solid red',
+    width: '1440px',
+    height: '120px',
+    border: '1px solid red',
+    marginRight: 'auto',
+    marginLeft: 'auto',
+  },
+  header: {
+    border: '1px solid black',
+    width: '100%',
+  },
+  content: {
+    border: '1px solid black',
+    width: '1200px',
+    marginRight: 'auto',
+    marginLeft: 'auto',
+  },
 });
